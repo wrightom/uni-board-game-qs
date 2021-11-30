eaae59b45a26f6e8e6182d930383885e92259405
Tom Wright <tomnwright@outlook.com>
2021-11-30 07:30:57 +0000

    initial

---
+++ src/AnswerBox.js
@@ -0,0 +1,7 @@
+function AnswerBox () {
+    return (
+        <div className='test'></div>
+    )
+}
+
+export default AnswerBox;
--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
 
+import AnswerBox from './AnswerBox.js';
+
 function App() {
   return (
     <div className="App">
@@ -9,6 +11,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <div>tese</div>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -17,6 +20,7 @@ function App() {
         >
           Learn React
         </a>
+        <AnswerBox />
       </header>
     </div>
   );
