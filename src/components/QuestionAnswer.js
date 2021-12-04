f121d0d57a7ede987250b38d16db6c7d0388e156
Tom Wright <tomnwright@outlook.com>
2021-12-04 01:44:58 +0000

    FInal

--- src/components/QuestionAnswer.js
+++ src/components/QuestionAnswer.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { MathJax, MathJaxContext } from "better-react-mathjax";
 
 function QuestionAnswer(props) {
     
@@ -12,9 +13,9 @@ function QuestionAnswer(props) {
     );
 
     return (
-        <>
+        <MathJaxContext>
             <div className="question-container">
-                <h1 className="no-margin">{props.prompt}</h1>
+                <h1 className="no-margin"><MathJax>{props.prompt}</MathJax></h1>
                 <p className="no-margin">{props.correctI === props.inputI? "Correct!! \u2705\u2705\u2705 Great job!":"Unlucky! \u{1F480}\u{1F614}"}</p>
                 <p className="no-margin">Press any button to continue</p>
             </div>
@@ -22,11 +23,11 @@ function QuestionAnswer(props) {
             <div className="answer-container">
                 {
                     props.answers.map((a,i)=>
-                        <button className={getButtonClass(i)} key={i} onClick={props.onContinue}>{a}</button>
+                        <button className={getButtonClass(i)} key={i} onClick={props.onContinue}><MathJax>{a}</MathJax></button>
                     )
                 }
             </div>
-        </>
+        </MathJaxContext>
     )
 }
 
--- src/components/QuestionInput.js
+++ src/components/QuestionInput.js
@@ -1,23 +1,38 @@
 import React from 'react'
+import { MathJax, MathJaxContext } from "better-react-mathjax";
 
 function QuestionInput(props) {
 
 
     const background_colours = ["#F7B724", "#6239DF", "#4BA6FF", "#CD4794"]
+    
+
+    const config = {
+        loader: { load: ["[tex]/html"] },
+        tex: {
+          packages: { "[+]": ["html"] },
+          inlineMath: [["$", "$"]],
+          displayMath: [["$$", "$$"]]
+        }
+      };
+
     return (
-        <>
+        <MathJaxContext config={config} version={3}>
+        
             <div className="question-container">
-                <h1 className="no-margin">{props.prompt}</h1>
+                
+                <h1 className="no-margin"><MathJax inline>{props.prompt}</MathJax></h1>
+                 
             </div>
 
             <div className="answer-container">
                 {
                     props.answers.map((a,i)=>
-                        <button className="clickable-btn answer-btn" onClick={()=>props.onAnswer(i)} style={{backgroundColor:background_colours[i]}}key={i}>{a}</button>
+                        <button className="clickable-btn answer-btn" onClick={()=>props.onAnswer(i)} style={{backgroundColor:background_colours[i]}}key={i}><MathJax>{a}</MathJax></button>
                     )
                 }
             </div>
-        </>
+        </MathJaxContext>
     )
 }
 
